9ede491346786244f56321ed57c3b2d2d91b8cbb
formularrr <447839267@qq.com>
2014-05-17 11:32:15 +0800

    Initial Commit

---
+++ microsoft100/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  microsoft100
+//
+//  Created by formula on 14-5-17.
+//  Copyright (c) 2014年 formula. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
